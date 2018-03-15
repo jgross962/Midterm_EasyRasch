708434552aaf13eb76e9de491e7423e85e012530
JGross <Jonathan.Gross@wustl.edu>
2018-03-14 19:55:09 -0500

    Converted probFunction to S4
    Converted probFunction to S4 methodology.

--- DevelopmentFile.R
+++ DevelopmentFile.R
@@ -11,7 +11,6 @@ library(roxygen2)
 
 # 1.) Write Code
 #6.) Can delete this after function is written into its own file
-
 # probFunction = function(raschObj, theta){
 #   P.vector = exp(theta-raschObj@a)/(1+exp(theta-raschObj@a))
 #   Q.vector = 1-P.vector
@@ -21,18 +20,18 @@ library(roxygen2)
 
 
 #3 Call This
-package.skeleton()
+#package.skeleton()
 
 #4 -- Make man and empty folder, just have functions your wrote in r; delete NameSpace
 ### Want: man (empty),R(with function),Description
 
 #5 update description file - package must match folderName
 
-#7. Add Roxygen (copy and paste from other files) and add as appropriate
+#7. Add Roxygen Documentation (copy and paste from other files) and add as appropriate
 
 #8. Use Roxygen to document
-setwd('C:/Users/jgros/Desktop/5625/Poisson_Master')
-current.code = as.package('poissonLikelihood')
+setwd('C:/Users/jgros/Documents/GitHub/Midterm_EasyRasch')
+current.code = as.package('easyRasch')
 load_all(current.code)
 document(current.code)
 
--- EasyRasch/R/probFunction.R
+++ EasyRasch/R/probFunction.R
@@ -5,22 +5,50 @@
 #' @param raschObj an object containing a students name, ability and whether he got a question correct
 #' @param theta a numeric value representing the ability of a student
 #'
-#' @return A data frame containing Probabiltiy of a correct answer(P.vector), and the probability of the students actual outcome (PQ)
+#' @return A data frame containing probability of a correct answer, and the probability of the students actual outcome
+#' \item \code{P.vector} a vector containing the probabilities the student gets a each correct
+#' \item \code{PQ} a vector containing the probabilities the student got the output he did
 #' @author Jonathan Gross <\email{jonathan.gross@@wustl.edu}>
 #' @note Add If desired
 #' @examples
+#' 
 #' testRaschObj = new("Rasch", name="Jon",a =c(2,4,3), y =c(1,1,0))
 #' theta = 3.5
 #' probFunction(test1,theta)
 #' @rdname probFunction
-#' @export
-probFunction <-
-function(raschObj, theta){
-  # Probability as calculated by Eq 1
-  P.vector = exp(theta-raschObj@a)/(1+exp(theta-raschObj@a)
-  Q.vector = 1-P.vector
+#' @aliases probFunction
 
-  PQ = P.vector 
-  PQ[which(y==1)] = Q.vector[which(y==1)]
-  return(data.frame(P,PQ))
-}
+#' @export
+setGeneric(name="probFunction",
+           def=function(raschObj = "Rasch",
+                        theta = "numeric",
+           ){
+             standardGeneric("probFunction")
+           })
+
+#' @export
+setMethod(f="probFunction",
+          definition=function(raschObj="Rasch",theta="numeric"){
+              # Probability as calculated by Eq 1
+              P.vector = exp(theta-raschObj@a)/(1+exp(theta-raschObj@a)
+              Q.vector = 1-P.vector
+
+              PQ = P.vector
+              PQ[which(y==1)] = Q.vector[which(y==1)]
+              return(data.frame(P.vector,PQ))
+            
+          }
+)
+
+
+## IF S3 Instead
+# probFunction <-
+# function(raschObj, theta){
+#   # Probability as calculated by Eq 1
+#   P.vector = exp(theta-raschObj@a)/(1+exp(theta-raschObj@a)
+#   Q.vector = 1-P.vector
+# 
+#   PQ = P.vector 
+#   PQ[which(y==1)] = Q.vector[which(y==1)]
+#   return(data.frame(P.vector,PQ))
+# }
